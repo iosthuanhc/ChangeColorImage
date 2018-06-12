d4842bf3cd04b5359d6b04ee5fd990f6ff06aa8b
Hà Công Thuận <thuan.hc@3si.vn>
2018-06-12 11:15:20 +0700

    Initial Commit

---
+++ ChangeColorImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChangeColorImage
+//
+//  Created by Ha Cong Thuan on 6/12/18.
+//  Copyright © 2018 Ha Cong Thuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
